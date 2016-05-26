28e3308487b12652abbdef3b29f152bbe926a5c1
Mikael Lindqvist <li.mikael@gmail.com>
2016-05-26 10:19:14 +0300

    initial

---
+++ mickesplugin.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name:       Micke's Plugin
+ * Plugin URI:        https://github.com/limikael/mickesplugin
+ * Description:       A plugin that does nothing useful, which is useful for testing.
+ * Version:           0.0.1
+ * Author:            Mikael Lindqvist
+ * License:           GNU General Public License v2
+ * GitHub Plugin URI: https://github.com/limikael/mickesplugin
+ */
+
+function mickesplugin_page() {
+	echo "<h1>Micke's Plugin</h1>";
+	echo "<p>Hello and welcome to Micke's Plugin.</p>";
+	echo "<p>We hope that you enjoy your experience.</p>";
+}
+
+function mickesplugin_admin_menu() {
+	add_menu_page(
+		"Micke's Plugin",
+		"Micke's Plugin",
+		"manage_options",
+		"mickesplugin",
+		"mickesplugin_page"
+	);
+}
+
+add_action("admin_menu","mickesplugin_admin_menu");
